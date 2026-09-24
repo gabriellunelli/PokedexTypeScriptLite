5e8ab92899f68c0ad951a4d9323fdad624d3f634
gabriellunelli <gabriellunelli1203@gmail.com>
2026-09-24 18:55:44 -0300

    feat: formatador de textos

---
+++ src/utils/textFormatters.ts
@@ -0,0 +1,11 @@
+export function aviso(texto:string):string {
+    return `[ AVISO ] ${texto}`
+}
+
+export function ok(texto:string):string {
+    return `[ OK ] ${texto}`
+}
+
+export function erro(texto:string):string {
+    return `[ ERRO ] ${texto}`
+}
